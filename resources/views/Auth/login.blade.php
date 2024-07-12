1756cc86b0572587c8aa46363faa96ac63ee39dc
Jssteban <stebanserrato7@gmail.com>
2024-07-11 20:11:55 -0500

    vistas realizadas

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -5,37 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>iniciar sesion</title>
+    <title>Iniciar sesión</title>
 </head>
-<body>
-    <style>
-        body {
-            background-color: #BFBFBF;
-        }
-    </style>
-    
-    <div class="container mt-5">
-        <h1 class="mb-4">User</h1>
-        <form action="" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="email" class="form-label">Iniciar sesión</label>
-                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
-                @error('email')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
+<body class="bg-dark text-white d-flex align-items-center justify-content-center min-vh-100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="card bg-dark border-light">
+                    <div class="card-body">
+                        <h1 class="text-center mb-4 text-danger">User</h1>
+                        <form action="" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="email" class="form-label text-white">Iniciar sesión</label>
+                                <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" value="{{ old('email') }}">
+                                @error('email')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label text-white">Contraseña</label>
+                                <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                                @error('password')
+                                    <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="d-grid">
+                                <button type="submit" class="btn btn-danger rounded-pill">Entrar</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Contraseña</label>
-                <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
-                @error('password')
-                    <div class="invalid-feedback">{{ $message }}</div>
-                @enderror
-            </div>
-            <button type="submit" class="btn btn-primary">Entrar</button>
-        </form>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </body>
 </html>
